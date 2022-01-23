9d1a5376f7ee7986334a9455e3108ffccd76f30f
jyotiprakash-m <teachmeonline365@gmail.com>
2022-01-23 22:29:16 +0530

    all done

---
+++ client/src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import 'antd/dist/antd.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import TokeIdComponent from './TokeIdComponent';
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode ho="true" >
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path=':tokenId' element={<TokeIdComponent />} />
+      </Routes>
+
+      {/* <App /> */}
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
